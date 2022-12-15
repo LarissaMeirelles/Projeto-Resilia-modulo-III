88e614e0107326261707db50ae4aa153244fd004
Larissa Meirelles <113534712+LarissaMeirelles@users.noreply.github.com>
2022-12-15 18:19:42 -0300

    Cards- commit final e atualizado.

---
+++ Larissa Meirelles- Cards/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Cards from './componentes/Cards/Cards';
+
+function App() {
+
+
+  return (
+
+    <div>
+     <Cards />   
+     
+      </div>
+
+  )
+}
+
+export default App
